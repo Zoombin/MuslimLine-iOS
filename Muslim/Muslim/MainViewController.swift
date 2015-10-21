dbc6d6b5a2a47179d581cabe6ec591fa4126ed0b
yc <ycabc1989@163.com>
2015-10-21 16:23:59 +0800

    提交工程

---
+++ Muslim/Muslim/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Muslim
+//
+//  Created by 颜超 on 15/10/21.
+//  Copyright © 2015年 ZoomBin. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
