c72465653eb1095ee07e75034ea1a80c16f7dfe0
yc <ycabc1989@163.com>
2015-10-29 18:10:08 +0800

    右上角加入菜单按钮

--- Muslim/Muslim/MainViewController.swift
+++ Muslim/Muslim/MainViewController.swift
@@ -12,15 +12,29 @@ class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //注: 设置title25 154 76
+        //注: 设置title
         title = "Muslim Line";
-        self.view.backgroundColor = UIColor(colorLiteralRed: 25/255.0, green: 154/255.0, blue: 76/255.0, alpha: 1.0)
-        // Do any additional setup after loading the view, typically from a nib.
-        initBottomView();
+        let greenColor : UIColor = UIColor(colorLiteralRed: 25/255.0, green: 154/255.0, blue: 76/255.0, alpha: 1.0)
+        
+        //设置标题栏颜色
+        self.navigationController?.navigationBar.barTintColor = greenColor
+        //设置标题的字的颜色
+        self.navigationController?.navigationBar.titleTextAttributes = NSDictionary(object: UIColor.whiteColor(),forKey: NSForegroundColorAttributeName) as? [String : AnyObject]
+        
+        self.view.backgroundColor = greenColor
+        
+        let rightImage : UIImage =  UIImage(named: "top_menu")!.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem.init(image: rightImage, style: UIBarButtonItemStyle.Plain, target: self, action: Selector.init("menuButtonClicked"))
+        initBottomView()
+    }
+    
+    func menuButtonClicked() {
+        
     }
     
     func initBottomView() {
         //注: var 和 let的区别， var是变量 let是常量
+        //let Object : 类型 比如CGFloat NSInterger等等
         let width : CGFloat = UIScreen.mainScreen().bounds.size.width
         let height : CGFloat = UIScreen.mainScreen().bounds.size.height
         let buttonWidth = width / 3
@@ -29,6 +43,8 @@ class MainViewController: UIViewController {
         var row : CGFloat = 0
         //第几个
         var position : CGFloat = 0
+        
+        //定义数组 [String] 代表数组里面的String的
         let titles : [String] = ["古兰经", "天房方向", "礼拜时间", "附近位置", "日历", "真主尊名"]
         let actions : [String] = ["clickGuLj", "clickTianFFX", "clickLiBSJ", "clickFuJWZ", "clickRiL", "clickZunZXM"]
         let imageNames : [String] = ["quran", "qibla", "prayer", "nearby", "calenlar", "names"]
@@ -42,8 +58,8 @@ class MainViewController: UIViewController {
             button.frame = CGRectMake(position * buttonWidth, (height / 2) + (row * buttonHeight), buttonWidth, buttonHeight)
             button.backgroundColor = UIColor.whiteColor()
             button.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
-            button.layer.borderWidth = 0.5
-            button.layer.borderColor = UIColor.lightGrayColor().CGColor
+            button.layer.borderWidth = 0.5 //设置边框的宽度
+            button.layer.borderColor = UIColor.lightGrayColor().CGColor //设置边框的颜色
             button.setImage(UIImage(named: imageNames[index]), forState: UIControlState.Normal)
             button.tag = index + 1
             button.addTarget(self, action: Selector.init(actions[index]), forControlEvents: UIControlEvents.TouchUpInside)
