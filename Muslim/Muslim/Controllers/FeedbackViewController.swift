a4ad8a4a19120eb7e800e3fe2a8b8b80a6fc7ab0
LSD <renwfy@126.com>
2015-10-30 22:17:07 +0800

    添加反馈页面

---
+++ Muslim/Muslim/Controllers/FeedbackViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedbackViewController.swift
+//  Muslim
+//
+//  Created by LSD on 15/10/30.
+//  Copyright © 2015年 ZoomBin. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
