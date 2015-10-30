8870c1da32dd860a8ebac1f4b87a3744942b7eb3
yc <ycabc1989@163.com>
2015-10-30 18:10:09 +0800

    更改about的文字

--- Muslim/Muslim/Controllers/AboutViewController.swift
+++ Muslim/Muslim/Controllers/AboutViewController.swift
@@ -13,7 +13,7 @@ class AboutViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //国际化
-        title = NSLocalizedString("About", comment:"")
+        title = NSLocalizedString("about", comment:"")
         
         let greenColor : UIColor = UIColor(colorLiteralRed: 25/255.0, green: 154/255.0, blue: 76/255.0, alpha: 1.0)
         //设置标题栏颜色
