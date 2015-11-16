d8c6179805b4a71e9321a561f80dc6938214ce87
LSD <renwfy@126.com>
2015-11-16 15:24:25 +0800

    删除SQLlite库，不支持7.0

---
+++ Muslim/Muslim/Utils/SQLiteDBHelper.swift
@@ -0,0 +1,13 @@
+//
+//  SQLiteDBHelper.swift
+//  Muslim
+//
+//  Created by LSD on 15/11/16.
+//  Copyright © 2015年 ZoomBin. All rights reserved.
+//
+
+import UIKit
+
+class SQLiteDBHelper: NSObject {
+
+}
