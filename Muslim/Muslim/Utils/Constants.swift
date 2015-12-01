a8c0ea99f28baa81fed9503817429b064d753507
yc <ycabc1989@163.com>
2015-12-01 16:49:44 +0800

    去除高德地图

--- Muslim/Muslim/Utils/Constants.swift
+++ Muslim/Muslim/Utils/Constants.swift
@@ -9,10 +9,7 @@
 import UIKit
 
 class Constants: NSObject {
-    static let APIKey = "84cb993da727c58b8ce7911e7678e9ae"
-    
     static let ADJUSTTIME : Int = 60; //默认时间调整
-    
     static let downloadBaseUri = "http://www.csufo.com:8001/Public/res/quran/" //base下载地址
     static let downloadTranslationUri = downloadBaseUri + "translation/" //译文下载地址
     static let downloadSoundUri = downloadBaseUri + "adhan/" //响礼闹钟音乐下载地址
---
+++ Muslim/Muslim/Utils/LocalNoticationUtils.swift
@@ -0,0 +1,23 @@
+//
+//  LocalNoticationUtils.swift
+//  Muslim
+//
+//  Created by 颜超 on 15/12/1.
+//  Copyright © 2015年 ZoomBin. All rights reserved.
+//
+
+import UIKit
+
+
+class LocalNoticationUtils: NSObject {
+    
+    static func showLocalNotification() {
+        let localNotification = UILocalNotification()
+        localNotification.fireDate = NSDate(timeIntervalSinceNow: 5)
+        localNotification.alertBody = "new Blog Posted at iOScreator.com"
+        localNotification.timeZone = NSTimeZone.defaultTimeZone()
+        localNotification.applicationIconBadgeNumber = UIApplication.sharedApplication().applicationIconBadgeNumber + 1
+        
+        UIApplication.sharedApplication().scheduleLocalNotification(localNotification)
+    }
+}
